778cb3fee84f0d30e501c725bf1b457c9000e2c3
priyanshiisingh <priyanshianamika1815@gmail.com>
2022-06-14 23:39:26 +0530

    upto swagger

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodoModule } from './todo/todo.module';
 
+//db connectivity
+import { MongooseModule } from '@nestjs/mongoose';
+
 @Module({
-  imports: [TodoModule],
+  imports: [TodoModule, MongooseModule.forRoot('mongodb://localhost/nest')],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/todo/todo.service.ts
+++ src/todo/todo.service.ts
@@ -1,4 +1,38 @@
 import { Injectable } from '@nestjs/common';
+import { Todo, TodoDocument } from './schemas/todo.schema';
+import { UpdateTodoDto } from './dto/update-todo.dto';
+import { CreateTodoDto } from './dto/create-todo.dto';
+
+//db
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 
 @Injectable()
-export class TodoService {}
+export class TodoService {
+  constructor(
+    @InjectModel(Todo.name) private readonly model: Model<TodoDocument>,
+  ) {}
+
+  async findAll(): Promise<Todo[]> {
+    return await this.model.find().exec();
+  }
+
+  async findOne(id: string): Promise<Todo> {
+    return await this.model.findById(id).exec();
+  }
+
+  async create(createTodoDto: CreateTodoDto): Promise<Todo> {
+    return await new this.model({
+      ...createTodoDto,
+      createdAt: new Date(),
+    }).save();
+  }
+
+  async update(id: string, updateTodoDto: UpdateTodoDto): Promise<Todo> {
+    return await this.model.findByIdAndUpdate(id, updateTodoDto).exec();
+  }
+
+  async delete(id: string): Promise<Todo> {
+    return await this.model.findByIdAndDelete(id).exec();
+  }
+}
